5e29d917038704528efce1d6eab1f09410b9f778
steven <chunyuko85@gmail.com>
2018-01-10 15:16:00 +0800

    account index,order locale

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -2,15 +2,15 @@ class AccountController < ApplicationController
   before_action :require_user
   before_action :get_cart_items
   before_action :load_base_cateogries
-  add_breadcrumb "首頁", :root_path
+  add_breadcrumb I18n.t("product.home"), :root_path
 
   def index
-    add_breadcrumb "帳號總覽", :account_path
+    add_breadcrumb t("account.overview"), :account_path
   end
 
   def edit
-    add_breadcrumb "帳號總覽", :account_path
-    add_breadcrumb "修改資料", :account_edit_path
+    add_breadcrumb t("account.overview"), :account_path
+    add_breadcrumb t("account.edit"), :account_edit_path
   end
 
   def orders
