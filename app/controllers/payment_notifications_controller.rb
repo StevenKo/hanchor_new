9e68e60fbf32a56f9c56dff27fcad379f80c1012
steven <chunyuko85@gmail.com>
2018-05-04 10:36:24 +0800

    fix paypal problems

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -5,12 +5,14 @@ class PaymentNotificationsController < ApplicationController
     Rails.logger.info("PARAMS: #{params.inspect}")
     if (params[:payment_status] == "Completed")
       order = Order.find(params[:invoice])
-      order.status = "pay_confirm"
-      order.is_show = true
-      order.is_payed = true
-      order.fill_fake_attribute
-      order.save
-      UserMailer.order_notification(order.user,order).deliver
+      if order.is_payed == false
+        order.status = "pay_confirm"
+        order.is_show = true
+        order.is_payed = true
+        order.fill_fake_attribute
+        order.save
+        UserMailer.order_notification(order.user,order).deliver
+      end
     elsif(params[:payment_status] == "Canceled_Reversal" || params[:payment_status] == "Denied")
       order = Order.find(params[:invoice])
       order.is_show = false
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -14,17 +14,11 @@ class Cart < ActiveRecord::Base
       :notify_url => notify_url,
       :charset => 'utf-8'
     }
-    cart_items.each_with_index do |item, index|
-      values.merge!({
-        "amount_#{index+1}" => item.price,
-        "item_name_#{index+1}" => item.product.product_infos.first.name,
-        "quantity_#{index+1}" => item.quantity
-      })
-    end
+    order = Order.find(order_id)
     values.merge!({
-      "amount_#{cart_items.size+1}" => shipping_cost.cost,
-      "item_name_#{cart_items.size+1}" => shipping_cost.description,
-      "quantity_#{cart_items.size+1}" => 1
+      "amount_1" => order.total,
+      "item_name_1" => "Total Price",
+      "quantity_1" => 1
     })
     "https://www.paypal.com/cgi-bin/webscr?" + values.to_query
   end
