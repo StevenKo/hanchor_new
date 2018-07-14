81692e3a1577092ffb0dfe98744a2a9cf326bf72
fifiten82726 <fifiteen82726@gmail.com>
2018-07-15 03:01:52 +0800

    Add: _comment and model method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,11 @@ class Comment < ApplicationRecord
   validates :rating, presence: true
   validates :name, presence: true
 
+
+  def self.compute_rating(comments_arr)
+    return 0 if comments_arr.blank?
+
+    rating = comments_arr.inject{ |sum, el| sum + el }.to_f / comments_arr.size
+    rating.round(1)
+  end
 end
