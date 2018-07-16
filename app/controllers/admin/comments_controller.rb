10b243b2983a13a05d9bb4d7050a7593f74e9b8d
fifiten82726 <fifiteen82726@gmail.com>
2018-07-16 17:00:17 +0800

    Add: search

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,12 @@
 class Admin::CommentsController < Admin::AdminController
   def index
-    @comments = Comment.order(id: :desc)
-                       .includes([product: :product_infos])
-                       .paginate(page: params[:page], per_page: 20)
+    @comments = Comment.order(id: :desc).includes([product: :product_infos])
+
+    if params[:search]
+      @comments = @comments.where('product_infos.name like ?', "%#{params[:search]}%")
+                           .references(:product_infos)
+    end
+
+    @comments = @comments.paginate(page: params[:page], per_page: 20)
   end
 end
-
-
-
