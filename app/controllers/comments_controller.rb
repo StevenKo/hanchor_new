ce7d58f2892571fe920ba333b27b5617d1de4ed7
fifiten82726 <fifiteen82726@gmail.com>
2019-01-23 15:04:07 +0800

    Add: email column

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(
       name: params[:name] || current_user.name,
+      email: params[:email] || current_user.email,
       rating: params[:rating].to_i,
       context: params[:context],
       product_id: params[:product_id].to_i,
